8e2eff5b952ac3f9496f142db0b9e52ddd119ade
Alexei1R <rusu.alexei.2002@gmail.com>
2023-11-14 13:46:19 +0200

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/toor/Desktop/Code/ATOM_Headless
+# Build directory: /home/toor/Desktop/Code/ATOM_Headless/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
